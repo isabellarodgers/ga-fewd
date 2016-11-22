ad9de31e8675495c8d47ddf847cea0f6e1e7c648
isabellarodgers <isabellarodgers@Isabellas-MacBook-Pro.local>
2016-11-22 18:40:54 -0500

    Week 5 Homework

---
+++ d10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,59 @@
+// console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var x = 0
+while (x < 4) {
+d.move ("right");
+x = x + 1
+}
+
+d.orient('down');
+
+var y = 0
+while (y < 16) {
+d.move ("right");
+y = y + 1
+}
+
+d.orient('up');
+
+d.orient('right')
+
+var x = 0
+while (x < 6) {
+d.move ("right");
+x = x + 1
+}
+
+d.orient('up');
+
+var y = 0
+while (y < 11) {
+d.move ("right");
+y = y + 1
+}
+
+d.orient("right");
+
+var x = 0
+while (x < 6) {
+d.move ("right");
+x = x + 1
+}
+
+d.orient("down");
+
+var y = 0
+while (y < 9) {
+d.move ("right");
+y = y + 1
+}
+
+d.orient("up");
+
+d.orient("right");
+
+var x = 0
+while (x < 2) {
+d.move ("right");
+x = x + 1
+}
